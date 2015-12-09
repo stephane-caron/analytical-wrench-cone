376e6411f0d8f0944983ffd204e45aafc9f663c0
Stephane Caron <caron@ynl.t.u-tokyo.ac.jp>
2015-12-09 20:18:20 +0900

    updated license disclaimers

--- dmotions/hrp4/topp/convert.py
+++ dmotions/hrp4/topp/convert.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015 Stephane Caron <caron@phare.normalesup.org>
+# Copyright (C) 2015 Stephane Caron <stephane.caron@normalesup.org>
 #
-# This file is part of surface-contacts-icra-2015.
+# This file is part of icra-2015.
 #
-# surface-contacts-icra-2015 is free software: you can redistribute it and/or
+# This code is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# surface-contacts-icra-2015 is distributed in the hope that it will be useful,
+# This code is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 # more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# surface-contacts-icra-2015. If not, see <http://www.gnu.org/licenses/>.
+# this code. If not, see <http://www.gnu.org/licenses/>.
+
 
 import dmotions
 import TOPP
